afb23a6f5f7b02250a0cd3c7a3e1905aaa6d32b8
Tom Stanley <tstanleyuk@icloud.com>
2019-07-22 19:23:39 +0800

    add loads of vars to the codebuild project module

--- modules/CodeBuild_Build_Project/main.tf
+++ modules/CodeBuild_Build_Project/main.tf
@@ -66,17 +66,6 @@ resource "aws_codebuild_project" "${var.project_name}" {
     image                       = "aws/codebuild/standard:1.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
-
-    environment_variable {
-      name  = "SOME_KEY1"
-      value = "SOME_VALUE1"
-    }
-
-    environment_variable {
-      name  = "SOME_KEY2"
-      value = "SOME_VALUE2"
-      type  = "PARAMETER_STORE"
-    }
   }
 
   logs_config {
@@ -89,61 +78,5 @@ resource "aws_codebuild_project" "${var.project_name}" {
 
   source {
     type            = "CODEPIPELINE"
-  }
-
-  vpc_config {
-    vpc_id = "vpc-725fca"
-
-    subnets = [
-      "subnet-ba35d2e0",
-      "subnet-ab129af1",
-    ]
-
-    security_group_ids = [
-      "sg-f9f27d91",
-      "sg-e4f48g23",
-    ]
-  }
-
-  tags = {
-    Environment = "Test"
-  }
-}
-
-resource "aws_codebuild_project" "project-with-cache" {
-  name          = "test-project-cache"
-  description   = "test_codebuild_project_cache"
-  build_timeout = "5"
-  service_role  = "${aws_iam_role.example.arn}"
-
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-
-  cache {
-    type  = "LOCAL"
-    modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
-  }
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:1.0"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-
-    environment_variable {
-      name  = "SOME_KEY1"
-      value = "SOME_VALUE1"
-    }
-  }
-
-  source {
-    type            = "GITHUB"
-    location        = "https://github.com/mitchellh/packer.git"
-    git_clone_depth = 1
-  }
-
-  tags = {
-    Environment = "Test"
   }
 }
